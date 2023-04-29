f26a29fc1e42e26d8854bf3fec8337d3bece8d46
Jovan Rodrigues <96989983+rodricksjo@users.noreply.github.com>
2023-04-30 01:10:07 +0530

    Delete web_page2 directory

--- web_page2/wb.py
+++
@@ -1,9 +0,0 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-
-# Set the server address and port
-server_address = ('localhost', 8001)
-httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
-
-# Start the server
-print('Server running at localhost:8001...')
-httpd.serve_forever()
